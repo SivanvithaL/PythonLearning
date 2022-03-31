e3517c5734df3b421998117c436863ae026eeda8
SivanvithaL <sivanvitha.lammata@gmail.com>
2022-03-31 11:19:19 +0200

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,33 @@
+mylist1 = [1,2,3]
+mylist2 = [4,5,6]
+print(mylist1)
+print(mylist2)
+#mylist1.append(mylist2)
+#print(mylist1)
+mylist1.extend(mylist2)
+print(mylist1)
+#print(sorted(mylist))
+#print(sorted(mylist, reverse = False))
+#mylist.remove(1)
+#print(mylist)
+#mylist.pop(0)
+#print(mylist)
+#mylist.append(4)
+#print(mylist)
+#mylist.append(1)
+#mylist.append(2)
+#mylist.append(3)
+#print(mylist[1:])
+#print(mylist[:-1])
+#print(mylist[-5:])
+# prints 1
+#print(mylist[1]) # prints 2
+#print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+#for x in mylist:
+ #   if 'a' == x:
+  #      print(x)
+        
+#print('6' in mylist)
+    
